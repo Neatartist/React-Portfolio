dea1679933d58b5e66d649b5b94026784a87309f
Daniel Van Kirk <davk0810@gmail.com>
2023-10-04 17:39:23 -0400

    Update skills.js

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,11 +1,15 @@
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import { Container, Col, Row } from "react-bootstrap";
-import meter1 from '../assets/images/meter1.png';
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp-min.png";
+
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -22,38 +26,38 @@ export const Skills = () => {
       items: 1
     }
   };
+
   return (
-    <section className="skill" id='skills'>
-      <Container>
-        <Row>
-          <Col>
-            <div className="skill-bx">
-              <h2>
-                Skills
-              </h2>
-              <p> Lorem Ipsum </p>
-              <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                <div className="item">
-                  <img src={} alt="Image"/>
-                  <h5>Web Development</h5>
+    <section className="skill" id="skills">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <h2>Skills</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>Javascript</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>React</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Object-Oriented Programming </h5>
+                            </div>
+                        </Carousel>
+                    </div>
                 </div>
-                <div className="item">
-                  <img src={} alt="Image"/>
-                  <h5>React</h5>
-                </div>
-                <div className="item">
-                  <img src={} alt="Image"/>
-                  <h5>Html</h5>
-                </div>
-                <div className="item">
-                  <img src={} alt="Image"/>
-                  <h5>Javascript</h5>
-                </div>
-              </Carousel>
             </div>
-          </Col>
-        </Row>
-      </Container>
+        </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   )
 }
