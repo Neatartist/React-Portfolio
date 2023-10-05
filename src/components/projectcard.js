81d5a253a8c16bcf9b07566bb0914a0e7f8e67a0
Daniel Van Kirk <davk0810@gmail.com>
2023-10-04 22:30:10 -0400

    Update projectcard.js

--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -1,14 +1,17 @@
-import {Col} from 'react-bootstrap';
-export const ProjectCard = ({ title, description, imgUrl }) => {
+import { Col } from 'react-bootstrap';
+
+export const ProjectCard = ({ title, description, imgUrl, URL }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl}/>
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+      <a href={URL} target="_blank" rel="noopener noreferrer"> 
+        <div className="proj-imgbx">
+          <img src={imgUrl} alt={title} />
+          <div className="proj-txtx">
+            <h4 className='white-text'>{title}</h4>
+            <span className='white-text'>{description}</span>
+          </div>
         </div>
-      </div>
+      </a>
     </Col>
-  )
-}
+  );
+};
