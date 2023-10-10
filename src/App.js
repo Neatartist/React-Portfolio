137c0b1ed763262725cfa2d9747627c5da7c3e0b
Daniel Van Kirk <davk0810@gmail.com>
2023-10-10 10:04:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
-import {NavBar} from './components/navbar';
-import {Banner} from './components/banner';
-import {Skills} from './components/skills';
-import {Projects} from './components/project';
-import {Footer} from './components/footer';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { NavBar } from './components/navbar';
+import { Banner } from './components/banner';
+import { Skills } from './components/skills';
+import { Projects } from './components/project';
+import { Contact } from './components/contact';
+import { Footer } from './components/footer';
+import {BrowserRouter as Router} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <Banner/>
-      <Skills/>
-      <Projects/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
